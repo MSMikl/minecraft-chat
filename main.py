412d653db4300c1b9b0495e7e23ad6bcb9396bcf
MSMikl <michalbl4@gmail.com>
2022-12-08 16:39:23 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import aiofiles
+import asyncio
+import datetime
+
+
+async def main():
+    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
+    message = 'hello'
+    while True:
+        async with aiofiles.open('log.txt', 'a') as file:
+            data = await reader.readline()
+            text_line = f"[{datetime.datetime.now().strftime('%d.%m.%y %H:%M')}] {data.decode('UTF-8')}"
+            await file.write(text_line)
+            print(text_line)
+
+asyncio.run(main())
