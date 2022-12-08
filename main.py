eee0721a280b31b10eeedb5eff7617ee5ff67849
MSMikl <michalbl4@gmail.com>
2022-12-08 17:04:34 +0600

    minor changes

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ async def main():
     argparser = configargparse.ArgParser(default_config_files=['config.ini'])
     argparser.add('-H', '--host', required=True, help='host')
     argparser.add('-p', '--port', required=True, help='port')
+    argparser.add('-l', '--logs', required=True, help='log file name')
     args = argparser.parse_args()
     reader, writer = await asyncio.open_connection(args.host, int(args.port))
     while True:
-        async with aiofiles.open('log.txt', 'a') as file:
+        async with aiofiles.open(args.logs, 'a') as file:
             data = await reader.readline()
             text_line = f"[{datetime.datetime.now().strftime('%d.%m.%y %H:%M')}] {data.decode('UTF-8')}"
             await file.write(text_line)
